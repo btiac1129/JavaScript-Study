a212a73bada028fc6bafa5fe18f64db655d9a97e
btiac1129 <btiac1129@gmail.com>
2021-08-26 13:48:42 +0900

    이벤트 버블링 활용

--- Design/VanillaJSComponent/src/components/Item.js
+++ Design/VanillaJSComponent/src/components/Item.js
@@ -20,18 +20,35 @@ export default class Items extends Component {
         `
     }
 
+    /* 
+        이벤트 버블링을 사용한다. 
+        모든 이벤트를 $target에 등록하여 사용하면 된다.
+    */
     setEvent () {
-        this.$target.querySelector(".addBtn").addEventListener("click", () => {
-            const { items } = this.$state;
-            this.setState({ items: [ ...items, `item${items.length + 1}`]});
-        });
+        // this.$target.querySelector(".addBtn").addEventListener("click", () => {
+        //     const { items } = this.$state;
+        //     this.setState({ items: [ ...items, `item${items.length + 1}`]});
+        // });
+        
+        // this.$target.querySelectorAll(".deleteBtn").forEach(deleteBtn => {
+        //     deleteBtn.addEventListener("click", ({ target }) => {
+        //         const items = [ ...this.$state.items ];
+        //         items.splice(target.dataset.index, 1); // (item 버튼의 key 값을 이용하여) 삭제만 한다.
+        //         this.setState({ items });
+        //     })
+        // })
 
-        this.$target.querySelectorAll(".deleteBtn").forEach(deleteBtn => {
-            deleteBtn.addEventListener("click", ({ target }) => {
-                const items = [ ...this.$state.items ];
-                items.splice(target.dataset.index, 1); // (item 버튼의 key 값을 이용하여) 삭제만 한다.
+        this.$target.addEventListener("click", ({ target }) => {
+            const items = [ ...this.$state.items ];
+
+            if (target.classList.contains("addBtn")) {
+                this.setState({ items: [ ...items, `item${items.length + 1}`]});
+            }
+
+            if (target.classList.contains("deleteBtn")) {
+                items.splice(target.dataset.index, 1);
                 this.setState({ items });
-            })
-        })
+            }
+        });
     }
 }
